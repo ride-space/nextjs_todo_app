66307f07693b45e454a82a2c6b45513c904bbe4e
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-03-05 21:26:52 +0900

    app runner test

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL_JSON: process.env.NEXT_PUBLIC_API_URL_JSON,
   },
 }
 
